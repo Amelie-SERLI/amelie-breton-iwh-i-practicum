72bd8a68a1db09da396f1dfce008eaabf978babc
Amelie-SERLI <130451741+Amelie-SERLI@users.noreply.github.com>
2025-07-30 17:39:08 +0200

    First commit to my Integrating With HubSpot I: Foundations practicum repository.
    step 1 : complete
    
    (added one custom object on hubspot to test)

--- index.js
+++ index.js
@@ -7,12 +7,34 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+require('dotenv').config();
+
+const PRIVATE_APP_ACCESS = process.env.PRACT_PRIV_APP;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+
+app.get('/', async (req, res) => {
+
+    const weapons = 'https://api.hubapi.com/crm/v3/objects/2-145281523?limit=10&archived=false&properties=name&properties=price&properties=use_description';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+
+    try {
+        const resp = await axios.get(weapons, { headers });
+        const data = resp.data.results;
+
+        console.log('Fetched weapons:', data.map(w => w.properties.name)); // fetch nothing
+
+        res.render('contacts', { title: 'Home | HubSpot APIs', weapons: data });      
+    } catch (error) {
+        console.error(error);
+    }
+
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -21,6 +43,8 @@ const PRIVATE_APP_ACCESS = '';
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+
+
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
